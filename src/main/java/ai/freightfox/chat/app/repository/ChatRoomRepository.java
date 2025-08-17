965ccb0093f90e3e550c1131269d763634a31a04
Gaurav Poudel <gaurair@Gauravs-MacBook-Air.local>
2025-08-17 18:09:10 +0530

    Bussiness Logic migrated from repository to service

--- src/main/java/ai/freightfox/chat/app/repository/ChatRoomRepository.java
+++ src/main/java/ai/freightfox/chat/app/repository/ChatRoomRepository.java
@@ -1,75 +1,60 @@
 package ai.freightfox.chat.app.repository;
 
-
-import ai.freightfox.chat.app.model.ChatRoom;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Set;
 
+@Slf4j
 @Repository
 public class ChatRoomRepository {
 
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
-    private final String BASE_KEY = "chatroom:";
 
-    public void saveChatRoom(ChatRoom chatRoom){
-        String hashKey = getRoomHashKey(chatRoom.getRoomName());
-        redisTemplate.opsForHash().put(hashKey, "createdAt", chatRoom.getCreatedAt());
-        redisTemplate.opsForHash().put(hashKey, "roomName", chatRoom.getRoomName());
-        redisTemplate.opsForHash().put(hashKey, "participantCount", String.valueOf(chatRoom.getParticipantCount()));
+    public void saveChatRoom(String chatRoomName, LocalDateTime createdAt, int participantCount, String hashKey){
+        redisTemplate.opsForHash().put(hashKey, "createdAt", createdAt);
+        redisTemplate.opsForHash().put(hashKey, "roomName", chatRoomName);
+        redisTemplate.opsForHash().put(hashKey, "participantCount", String.valueOf(participantCount));
     }
 
-    public Map<Object, Object> getChatRoomMetaData(String roomName){
-        String hashKey = getRoomHashKey(roomName);
+    public Map<Object, Object> getChatRoomMetaData(String hashKey){
         return redisTemplate.opsForHash().entries(hashKey);
     }
 
-    public boolean isRoomExist(String roomName){
-        String hashKey = getRoomHashKey(roomName);
+    public boolean isRoomExist(String hashKey){
         return redisTemplate.hasKey(hashKey);
     }
 
-    public String getRoomHashKey(String roomName){
-        return BASE_KEY + roomName;
-    }
-
-    public String getParticipantRoomHashKey(String roomName){
-        return BASE_KEY + roomName + ":participants";
-    }
-
-    public boolean addParticipant(String roomName, String participantName){
-        String participantRoomHashKey = getParticipantRoomHashKey(roomName);
-
+    public boolean addParticipant(String roomName, String participantName, String  participantRoomHashKey, String roomHashKey){
         if(!Boolean.TRUE.equals(redisTemplate.opsForSet().isMember(participantRoomHashKey, participantName))){
             redisTemplate.opsForSet().add(participantRoomHashKey, participantName);
-            String roomHashKey = getRoomHashKey(roomName);
             redisTemplate.opsForHash().increment(roomHashKey, "participantCount", 1);
             return true;
         }
         return false;
     }
 
-    public boolean removeParticipant(String participantName, String roomName){
-        String participantRoomHashKey = getParticipantRoomHashKey(roomName);
+    public boolean removeParticipant(String participantName, String roomName, String participantRoomHashKey, String roomHashKey){
         if(Boolean.TRUE.equals(redisTemplate.opsForSet().isMember(participantRoomHashKey, participantName))){
-            String roomHashKey = getRoomHashKey(roomName);
             redisTemplate.opsForHash().increment(roomHashKey, "participantCount", -1);
             redisTemplate.opsForSet().remove(participantRoomHashKey, participantName);
+            log.info("Participant: {} removed from room: {}",participantName, roomName);
             return true;
         }
+        log.info("Participant :{}  does not exist in room :{}", participantName, roomName);
         return false;
     }
 
-    public Set<Object> getParticipants(String roomName) {
-        String participantRoomHashKey = getParticipantRoomHashKey(roomName);
+    public Set<Object> getParticipants(String roomName, String participantRoomHashKey) {
         return redisTemplate.opsForSet().members(participantRoomHashKey);
     }
 
-    public boolean isParticipantInRoom(String roomName, String participant) {
-        String participantRoomHashKey = getParticipantRoomHashKey(roomName);
+    public boolean isParticipantInRoom(String participantRoomHashKey, String participant) {
         return Boolean.TRUE.equals(redisTemplate.opsForSet().isMember(participantRoomHashKey, participant));
     }
 
