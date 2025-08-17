6fee4cc5282005fd208db738c865747fbdf1b970
Gaurav Poudel <gaurair@Gauravs-MacBook-Air.local>
2025-08-17 13:13:50 +0530

    Freight Fox Chat app setup

---
+++ src/main/java/ai/freightfox/chat/app/FreightFoxApplication.java
@@ -0,0 +1,13 @@
+package ai.freightfox.chat.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreightFoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreightFoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/freightfox/chat/app/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.freightfox.chat.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
