cff31b01765624d9751d04c77487ec8076ff809f
Gaurav Poudel <gaurair@Gauravs-MacBook-Air.local>
2025-08-17 14:39:33 +0530

    DTO - Request and Response Class Created

---
+++ src/main/java/ai/freightfox/chat/app/dto/request/SendMessageRequest.java
@@ -0,0 +1,17 @@
+package ai.freightfox.chat.app.dto.request;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SendMessageRequest {
+    private String participant;
+    private String message;
+    private String roomName;
+}
---
+++ src/main/java/ai/freightfox/chat/app/dto/response/ApiResponse.java
@@ -0,0 +1,13 @@
+package ai.freightfox.chat.app.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+    private String message;
+    private String status;
+}
